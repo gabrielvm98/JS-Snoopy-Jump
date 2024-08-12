cf54cfd2b7f27749fa725c1107c29e94a775c814
gabrielvm98 <64874245+gabrielvm98@users.noreply.github.com>
2024-08-12 01:23:40 -0500

    Add Score and Mechanics

--- script.js
+++ script.js
@@ -31,6 +31,10 @@ let platformWidth = 60;
 let platformHeight = 18;
 let platformImg;
 
+let score = 0;
+let maxScore = 0;
+let gameOver = false;
+
 
 
 window.onload = function(){
@@ -66,6 +70,9 @@ window.onload = function(){
 
 function update() {
     requestAnimationFrame(update);
+    if(gameOver){
+        return;
+    }
     context.clearRect(0, 0, board.width, boardHeight);
 
 
@@ -79,6 +86,9 @@ function update() {
     }
     player.y += velocityY;
     velocityY += gravity;
+    if(player.y > board.height){
+        gameOver = true;
+    };
     context.drawImage(player.img, player.x, player.y, player.width, player.height);
 
 
@@ -86,11 +96,52 @@ function update() {
     for (let index = 0; index < platformArray.length; index++) {
         let platform = platformArray[index];
 
-        if (detectCollision(player, platform)){
-            velocityY = -8;
+        if (velocityY < 0 && player.y < boardHeight*11/16) {
+            platform.y += 4; //slide platform down
         }
 
-        context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);        
+        if (detectCollision(player, platform) && velocityY >= 0.4 && player.y >= 75){
+            if(platform.type == 0) {
+                velocityY = -8;
+            }
+            if(platform.type == 1) {
+                //velocityY = -16;
+            }
+            if(platform.type == 2) {
+                gameOver = true;
+            }
+        }
+
+        context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
+        if (platform.type == 2) {
+            // Cambia la operación de composición para el filtro rojo
+            context.globalCompositeOperation = 'source-atop';
+        
+            // Dibuja un rectángulo rojo semitransparente sobre la imagen
+            context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+            context.fillRect(platform.x, platform.y, platform.width, platform.height);
+        
+            // Restablece la operación de composición a la predeterminada
+            context.globalCompositeOperation = 'source-over';
+        }        
+    }
+
+    while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
+        if(platformArray[0].type == 0){
+            newPlatform(); 
+        }        
+        platformArray.shift(); //removes first element from the array        
+    }
+
+
+    updateScore();
+    context.fillStyle = "black";
+    context.font = "16px sans-serif";
+    context.fillText(score, 5, 20);
+
+    if (gameOver) {
+        context.fillText("Game Over", boardWidth*2.5/7, boardHeight*13/16);
+        context.fillText("Press 'Space' or Refresh to Restart", boardWidth/7, boardHeight*7/8);
     }
 }
 
@@ -104,6 +155,23 @@ function movePlayer(e) {
     else if(e.code == "ArrowLeft" || e.code == "KeyA"){
         velocityX = -4;
         player.img = playerLeftImg;
+    }
+    else if (e.code == "Space" && gameOver) {
+        player = {
+            img : playerRightImg,
+            x : playerX,
+            y : playerY,
+            width : playerWidth,
+            height : playerHeight
+        }
+        velocityX = 0;
+        velocityY = -8;
+
+        score = 0;
+        maxScore = 0;
+        gameOver = false;
+
+        placePlatforms();
     }
 }
 function movePlayerTouch(e) {
@@ -130,23 +198,67 @@ function placePlatforms() {
         height : platformHeight,
         type : 0
     }
-    platformArray.push(platform);
+    platformArray.push(platform);    
 
-    platform = {
+    for (let index = 0; index < 6; index++) {
+        let randomX = Math.floor(Math.random() * boardWidth * 3/4);
+
+        let platform = {
+            img : platformImg,
+            x : randomX,
+            y : boardHeight - 75*index - 150,
+            width : platformWidth,
+            height : platformHeight,
+            type : 0
+        }    
+        platformArray.push(platform);
+    }
+}
+
+function newPlatform() {
+    let randomX = Math.floor(Math.random() * boardWidth * 3/4);
+
+    let platform = {
         img : platformImg,
-        x : boardWidth/4,
-        y : boardHeight - 150,
+        x : randomX,
+        y : -platformHeight,
         width : platformWidth,
         height : platformHeight,
         type : 0
-    }
+    }    
     platformArray.push(platform);
+
+    let chance = Math.random()
+    if(chance<=0.20){
+        randomX = Math.floor(Math.random() * boardWidth * 3/4);
+        let platformX = {
+            img : platformImg,
+            x : randomX,
+            y : -platformHeight - 36,
+            width : platformWidth,
+            height : platformHeight,
+            type : 2
+        }    
+        platformArray.push(platformX);
+    }
 }
 
 function detectCollision(a ,b) {
     return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
            a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
            a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-           a.y + a.height > b.y &&  //a's bottom left corner passes b's top left corner
-           velocityY >= 0;
+           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+}
+
+function updateScore() {
+    let points = 10*Math.floor(5*Math.random()); 
+    if (velocityY < 0) { //negative going up
+        maxScore += points;
+        if (score < maxScore) {
+            score = maxScore;
+        }
+    }
+    else if (velocityY >= 0) {
+        maxScore -= points;
+    }
 }
